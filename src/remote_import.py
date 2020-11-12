8d9c7e4cd819b133d61306b77767a62e74fe46fa
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-11-12 18:11:31 +0200

    preview with error

--- src/remote_import.py
+++ src/remote_import.py
@@ -1,5 +1,7 @@
 import os
 from urllib.parse import urlparse
+import htmllistparse
+
 import supervisely_lib as sly
 
 
@@ -17,15 +19,44 @@ def start_import(api: sly.Api, task_id, context, state, app_logger):
 @my_app.callback("preview_remote")
 @sly.timeit
 def preview_remote(api: sly.Api, task_id, context, state, app_logger):
-    remote_dir = state["remoteDir"]
-    parts = urlparse(remote_dir)
-    project_name = parts.path.rstrip("/")
-    if project_name not in ["", "/"]:
-        project_name = sly.fs.get_file_name(project_name) # last directory name from path
-    else:
-        project_name = ""
-    fields = [{"field": "state.projectName", "payload": project_name}, ]
-    api.app.set_fields(task_id, fields)
+    api.task.set_field(task_id, "data.previewError", "")
+    try:
+        remote_dir = state["remoteDir"]
+        parts = urlparse(remote_dir)
+        project_name = parts.path.rstrip("/")
+        if project_name not in ["", "/"]:
+            project_name = sly.fs.get_file_name(project_name) # last directory name from path
+        else:
+            project_name = ""
+
+        cwd, raw_listing = htmllistparse.fetch_listing(remote_dir, timeout=30)
+
+        listing = []
+        dataset_names = []
+        meta_json_exists = False
+        for file_entry in raw_listing:
+            name = file_entry.name
+            if name == 'meta.json':
+                meta_json_exists = True
+                listing.append({"name": name, "selected": True, "disabled": True})
+            elif name.endswith("/"):
+                listing.append({"name": name, "selected": True, "disabled": False})
+                dataset_names.append(name.rstrip("/"))
+            else:
+                app_logger.info("Skip file {!r}".format(os.path.join(remote_dir, name)))
+                listing.append({"name": name, "selected": False, "disabled": True})
+
+        #if meta_json_exists is False:
+        raise FileNotFoundError("meta.json")
+
+        fields = [
+            {"field": "state.projectName", "payload": project_name},
+            {"field": "state.listing", "payload": listing},
+        ]
+        api.app.set_fields(task_id, fields)
+    except Exception as e:
+        api.task.set_field(task_id, "data.previewError", repr(e))
+
 
 
 @my_app.callback("preprocessing")
@@ -46,14 +77,18 @@ def main():
         "uploadedCount": 0,
         "totalCount": 0,
         "uploadProgress": 0,
-        "uploadError": ""
+        "uploadError": "",
+        "taskId": my_app.task_id,
+        "previewError": ""
     }
 
     state = {
-        "remoteDir": "http://localhost:8088/my_sly_project/",
+        #"remoteDir": "http://localhost:8088/my_sly_project/",
+        "remoteDir": "http://172.20.10.2:8088/my_sly_project/",
         "teamName": team.name,
         "workspaceName": workspace.name,
-        "projectName": ""
+        "projectName": "",
+        "listing": []
     }
 
     # Run application service
